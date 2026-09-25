f16c730c3589e0ce999016316c47af4ff9fa307c
Divyansh Upadhyay <155802095+divyansh-techy@users.noreply.github.com>
2026-09-25 19:58:00 +0530

    Update smart_expense_tracker.sql

--- database/smart_expense_tracker.sql
+++ database/smart_expense_tracker.sql
@@ -1,1 +1,21 @@
+// Creating DATABASE
+CREATE DATABASE smart_expense_tracker;
 
+USE smart_expense_tracker;
+
+// Users table
+CREATE TABLE users (
+    user_id INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(100) NOT NULL
+);
+
+// Expenses table
+CREATE TABLE expenses (
+    expense_id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    category VARCHAR(50) NOT NULL,
+    amount DOUBLE NOT NULL,
+    description VARCHAR(200),
+    expense_date DATE NOT NULL
+);
